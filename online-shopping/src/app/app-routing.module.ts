1636478f931471a0803d0f67346900389dc8206b
JAPJEET SINGH <japjeet612003@gmail.com>
2023-08-09 16:40:28 +0530

    update

---
+++ online-shopping/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './layout/layout.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  {path:"", redirectTo:"layout/home" , pathMatch:'full'},
+  {path:"layout", component:LayoutComponent,
+  children:[
+    {path:"home", component:HomeComponent},
+    {path:"about", component:AboutComponent}
+
+  ]
+
+}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
